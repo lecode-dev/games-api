327f6dfa68006f429393e81970f178daee809cbf
Pablo Felipe <pablofelipe1999@hotmail.com>
2022-01-14 15:37:16 -0300

    Adding Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Footer from './components/Footer';
 import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <NavBar/>
+    <body>
+      <NavBar/>
+      <Footer/>
+    </body>
   );
 }
 
