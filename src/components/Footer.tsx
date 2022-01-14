d1469df56f1e165ff9aa3093226ca6582c144089
Pablo Felipe <pablofelipe1999@hotmail.com>
2022-01-14 15:38:23 -0300

    Changing date and project name

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -63,7 +63,7 @@ const navigation = [
             ))}
           </div>
           <div className="mt-8 md:mt-0 md:order-1">
-            <p className="text-center text-base text-gray-400">&copy; 2020 Workflow, Inc. All rights reserved.</p>
+            <p className="text-center text-base text-gray-400">&copy; 2022 The Game Database, LeCode. All rights reserved.</p>
           </div>
         </div>
       </footer>
